20c32bc28fbac34bd359e6b73907a15f7949b10d
Arseniy Simonov <catterer@yandex.ru>
2016-12-12 19:56:14 +0000

    tests

---
+++ tests/test_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run_test {
+    NAME=$1
+    cat $NAME.in  | ../jabz  | hexdump -C > tmp
+    RES=`diff tmp $NAME.out | wc -l`
+    if [[ "$RES" -ne "0" ]]
+    then
+        echo "Failed    $NAME"
+        cp tmp $NAME.failed
+    else
+        echo "OK        $NAME"
+    fi
+    rm tmp
+}  
+
+run_test 1
